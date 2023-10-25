8947f035c1e84b9acd27c496426dcbf2e95c0ef8
DCKAP <vijayalakshmigdckap@gmail.com>
2023-10-25 23:01:53 +0530

    stone-paper-scissor added

---
+++ demo.js
@@ -0,0 +1,101 @@
+// let player1=prompt("Enter a player1 value");
+// let player2=prompt("Enter a player2 value");
+
+// player1=player1.trim().toLowerCase();
+// player2=player2.trim().toLowerCase();
+
+// let a="paper"
+// let b="scissor"
+// let c="stone"
+
+// let valid = ((player1 == a || player1 == b || player1 == c) &&
+//             (player2 == a || player2 == b || player2 == c));
+
+// if((player1==0) || (player2==0))
+// {
+//     alert("invalid input")
+// }
+
+// else if(((player1==b) && (player2==a)) || 
+//         ((player1==c) && (player2==b)) || 
+//         ((player1==a) && (player2==c)))
+// {
+//     alert("player1 is winner");
+// }
+
+// else if((player1==b) && (player2==c)|| 
+//         (player1==c) && (player2==a)|| 
+//         (player1==a) &&(player2==b))
+// {
+//     alert("player2 is winner")
+// }
+
+// else if((player1.match(a)==null) || 
+//         (player1.match(b)==null) ||
+//         (player1.match(c)==null) ||
+//         (player2.match(a)==null) || 
+//         (player2.match(b)==null) ||
+//         (player2.match(c)==null))
+// {
+//     alert("invalid value")
+// }
+
+// else
+// {
+//     alert("match draw")
+// }
+
+
+
+
+
+
+// else if((player1=="stone") && (player2=="scissor"))
+// {
+//     alert("player1 is winner");
+// }
+
+// else if((player1=="scissor") && (player2=="stone"))
+// {
+//     alert("player2 is winner");
+// }
+
+// else if((player1=="paper") && (player2=="stone"))
+// {
+//     alert("player1 is winner");
+// }
+
+// else if((player1=="stone") && (player2=="stone"))
+// {
+//     alert("match draw");
+// }
+
+// else if((player1=="paper") && (player2="paper"))
+// {
+//     alert("match draw");
+// }
+
+// else if((player1=="scissor") && (player2=="scissor"))
+// {
+//     alert("match draw");
+// }
+
+// let a=0;
+
+// for(i=1;i<10;i++)
+// {
+//     if(i%3==0 || i%5==0)
+//     {
+//         a *= i;
+
+//     }
+// }
+// console.log(a)
+
+
+// let a=2
+// let b=10
+
+// a*=b
+
+// console.log(a)
